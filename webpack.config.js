b1e7239a723efbfee08bb4f6d9554d336a03bad7
halufun <frogillius@hotmail.com>
2025-04-22 20:11:50 -0700

    extras in the config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
-  mode: 'development', // or 'production'
+  mode: 'development',            // or 'production'
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: './public/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
 };
