572fa2fd085b46643293abb4034528230016bd2b
halufun <frogillius@hotmail.com>
2025-04-22 20:56:58 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
+// webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: './public/main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,               // wipes dist/ on each build
   },
-  
+  module: {
+    rules: [
+      {
+        test: `/\.css$/i`,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: `/\.(png|jpg|svg|gif|woff2?)$/i`,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',  // your HTML template
+      inject: 'body',                   // where to inject <script>
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/static', to: 'static' }, // copy all files under public/static
+      ],
+    }),
+  ],
 };
