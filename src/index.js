042d753d8b2df18d6bff5c08ecfb0073c929cc4f
halu <151787619+halufun@users.noreply.github.com>
2025-04-28 10:23:38 -0700

    fix supabase import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// database url:
-const url = "https://bmkocgtuwuwdoakptqan.supabase.co";
-// public api
-const api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJta29jZ3R1d3V3ZG9ha3B0cWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDk1NzAsImV4cCI6MjA2MDIyNTU3MH0.Hi5KxIT8T9jED2JbHO25TcdUcBFDfCPHgPxOttjezEo";
-// supabase instance
-const supabase = createClient(url, api);
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://bmkocgtuwuwdoakptqan.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJta29jZ3R1d3V3ZG9ha3B0cWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDk1NzAsImV4cCI6MjA2MDIyNTU3MH0.Hi5KxIT8T9jED2JbHO25TcdUcBFDfCPHgPxOttjezEo';
+const supabase = createClient(supabaseUrl, supabaseKey);// database url:
 
 async function insertData() {
     const { data, error } = await supabase
