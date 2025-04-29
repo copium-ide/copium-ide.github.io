afaf5a53ef4b885d6155cb876bad857b84efc1e8
halu <151787619+halufun@users.noreply.github.com>
2025-04-29 10:13:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const supabaseUrl = 'https://bmkocgtuwuwdoakptqan.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJta29jZ3R1d3V3ZG9ha3B0cWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDk1NzAsImV4cCI6MjA2MDIyNTU3MH0.Hi5KxIT8T9jED2JbHO25TcdUcBFDfCPHgPxOttjezEo';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function register(email, password) {
+async function register(email, password) {
     const { data, error } = await supabase.auth.signUp({
     email: email,
     password: password,
@@ -14,13 +14,25 @@ export async function register(email, password) {
   });
     console.log(data, error);
 }
-export async function login(email, password) {
+async function login(email, password) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
   });
     console.log(data, error);
 }
-export async function changePassword(password) {
+async function changePassword(password) {
     await supabase.auth.updateUser({ password: password })
 }
+const register = document.getElementById("register");
+  const login = document.getElementById("login");
+  const emailInput = document.getElementById("email");
+  const passInput = document.getElementById("password");
+  const email = emailInput.value;
+  const password = passInput.value;
+  register.addEventListener("click", function (e) {
+    account.register(email, password);
+  });
+  login.addEventListener("click", function (e) {
+    account.login(email, password);
+  });
