897630c06fcb940f86378edf2cbee7c64e92d6b2
halu <151787619+halufun@users.noreply.github.com>
2025-04-29 09:57:36 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const supabaseUrl = 'https://bmkocgtuwuwdoakptqan.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJta29jZ3R1d3V3ZG9ha3B0cWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDk1NzAsImV4cCI6MjA2MDIyNTU3MH0.Hi5KxIT8T9jED2JbHO25TcdUcBFDfCPHgPxOttjezEo';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function register(email, password) {
+export async function register(email, password) {
     const { data, error } = await supabase.auth.signUp({
     email: email,
     password: password,
@@ -14,13 +14,13 @@ async function register(email, password) {
   });
     console.log(data, error);
 }
-async function login(email, password) {
+export async function login(email, password) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
   });
     console.log(data, error);
 }
-async function changePassword(password) {
+export async function changePassword(password) {
     await supabase.auth.updateUser({ password: password })
 }
