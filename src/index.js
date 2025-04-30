498b7310d0d860c784f35995bb82f336ae71a776
halu <151787619+halufun@users.noreply.github.com>
2025-04-30 09:35:14 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const supabaseUrl = 'https://bmkocgtuwuwdoakptqan.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJta29jZ3R1d3V3ZG9ha3B0cWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDk1NzAsImV4cCI6MjA2MDIyNTU3MH0.Hi5KxIT8T9jED2JbHO25TcdUcBFDfCPHgPxOttjezEo';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+async function handleSignInWithGoogle(response) {
+  const { data, error } = await supabase.auth.signInWithIdToken({
+    provider: 'google',
+    token: response.credential,
+  })
+}
 async function register(email, password) {
     const { data, error } = await supabase.auth.signUp({
     email: email,
